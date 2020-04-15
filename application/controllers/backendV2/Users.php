be94278217bcda58b3625bd4efcd26227b21df78
Luan Nguyen <nhatlang19@gmail.com>
2020-04-15 18:21:05 +0700

    some changes

--- application/controllers/backendV2/Users.php
+++ application/controllers/backendV2/Users.php
@@ -35,6 +35,10 @@ class Users extends CI_Controller
 
     function index()
     {
+        print_r($this->session->userdata('logged_in'));
+        print(BACKEND_V2_TMPL_PATH);
+        exit;
+
         if ($this->session->userdata('logged_in')) {
             redirect(BACKEND_V2_TMPL_PATH . 'storage/lists');
         } else {
